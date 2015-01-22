e27024280fa8694caf6a1ff321252e796ebceeb3
Dario Gieselaar <d.gieselaar@gmail.com>
2015-01-22 11:48:50 +0100

    Refactored example.js

--- src/client/example.js
+++ src/client/example.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+	// Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 (function() {
 	"use strict";
 
@@ -19,9 +19,7 @@
 		$scope.projection = projectionFor($scope.configuration);
 
 		$scope.configuration.onChange(function() {
-			$scope.$applyAsync(function() {                             // this call is not tested
-				$scope.projection = projectionFor($scope.configuration);
-			});
+			$scope.projection = projectionFor($scope.configuration);
 		});
 	}]);
 
